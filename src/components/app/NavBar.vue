138dee242f97abfba740b4fc0c287d1859057811
ARTALEAL <artaleal@yandex.ru>
2023-12-24 00:50:25 +0300

    dropdown, date

--- src/components/app/NavBar.vue
+++ src/components/app/NavBar.vue
@@ -5,7 +5,7 @@
         <a href="#" @click.prevent="$emit('navbar-click')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{ dateFilter }}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -14,6 +14,7 @@
             class="dropdown-trigger black-text"
             href="#"
             data-target="dropdown"
+            ref="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
@@ -21,13 +22,13 @@
 
           <ul id="dropdown" class="dropdown-content">
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -39,7 +40,50 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: new Date(),
+      interval: null,
+      dropdown: null,
+      format: 'date-time',
+    };
+  },
+  methods: {
+    logout() {
+      console.log('logout');
+      this.$router.push('/login?message=logout');
+    },
+  },
+  mounted() {
+    this.interval = setInterval(() => (this.date = new Date()), 1000);
+    this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: true,
+    });
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy();
+    }
+  },
+  computed: {
+    dateFilter(value, format = this.format) {
+      const options = {};
+      if (format.includes('date')) {
+        options.day = '2-digit';
+        options.month = 'long';
+        options.year = 'numeric';
+      }
+      if (format.includes('time')) {
+        options.hour = '2-digit';
+        options.minute = '2-digit';
+        options.second = '2-digit';
+      }
+      return new Intl.DateTimeFormat('ru-RU', options).format(this.date);
+    },
+  },
+};
 </script>
 
 <style></style>
