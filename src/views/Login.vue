b584ba5955ab2ecae3fef0e7eff3552e63f9ad45
ARTALEAL <artaleal@yandex.ru>
2023-12-24 03:55:17 +0300

    incorrect validate

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,50 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="handleSubmit">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" class="validate" />
+        <input
+          v-model.trim="email"
+          id="email"
+          type="text"
+          class="validate"
+          :class="{
+            invalid:
+              (v$.email.$dirty && !v$.email.$required) ||
+              (v$.email.$dirty && !v$.email.email),
+          }"
+        />
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small v-if="!v$.email.$required" class="helper-text invalid"
+          >Введите email</small
+        >
+        <small v-else-if="!v$.email.$email" class="helper-text invalid"
+          >Введите корректный email</small
+        >
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input
+          id="password"
+          v-model="password"
+          type="password"
+          class="validate"
+          :class="{
+            invalid:
+              (v$.password.$dirty && !v$.password.$required) ||
+              (v$.password.$dirty && !v$.password.minLength),
+          }"
+        />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+          v-if="v$.password.$dirty && !v$.password.$required"
+          class="helper-text invalid"
+          >Введите пароль</small
+        >
+        <small
+          v-else-if="v$.password.$dirty && !v$.password.minLength"
+          class="helper-text invalid"
+          >Пароль должен быть не меньше 6-ти символов</small
+        >
       </div>
     </div>
     <div class="card-action">
@@ -23,8 +57,45 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
+
+<script>
+import { useVuelidate } from '@vuelidate/core';
+import { required, email, minLength } from '@vuelidate/validators';
+export default {
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  name: 'login',
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  validations() {
+    return {
+      email: { email, required },
+      password: { minLength: minLength(6), required },
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      const isFormCorrect = await this.v$.$validate();
+      if (!isFormCorrect) return;
+      this.$router.push('/');
+    },
+    // handleSubmit() {
+    //   if (this.v$.$invalid) {
+    //     this.v$.$touch();
+    //     return;
+    //   }
+    //   this.$router.push('/');
+    // },
+  },
+};
+</script>
