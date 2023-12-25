1befa3dc497d01cbb0b5d473b7b47225fd28b78a
ARTALEAL <artaleal@yandex.ru>
2023-12-25 23:26:53 +0300

    fix date render bug

--- src/components/app/NavBar.vue
+++ src/components/app/NavBar.vue
@@ -69,27 +69,29 @@ export default {
     }
   },
   computed: {
-    dateFilter(value, format = this.format) {
-      const options = {};
-      if (format.includes('date')) {
-        options.day = '2-digit';
-        options.month = 'long';
-        options.year = 'numeric';
-      }
-      if (format.includes('time')) {
-        options.hour = '2-digit';
-        options.minute = '2-digit';
-        options.second = '2-digit';
-      }
-      return new Intl.DateTimeFormat('ru-RU', options).format(this.date);
+    name: {
+      get() {
+        if (this.$store.getters.info.name) {
+          return this.$store.getters.info.name;
+        }
+        return 'User Name';
+      },
     },
-  },
-  computed: {
-    name() {
-      if (this.$store.getters.info.name) {
-        return this.$store.getters.info.name;
-      }
-      return 'User Name';
+    dateFilter: {
+      get(value, format = this.format) {
+        const options = {};
+        if (format.includes('date')) {
+          options.day = '2-digit';
+          options.month = 'long';
+          options.year = 'numeric';
+        }
+        if (format.includes('time')) {
+          options.hour = '2-digit';
+          options.minute = '2-digit';
+          options.second = '2-digit';
+        }
+        return new Intl.DateTimeFormat('ru-RU', options).format(this.date);
+      },
     },
   },
 };
